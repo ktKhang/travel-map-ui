1f0c8fe4ab62cc501a655217659d803930484e30
ktKhang <letankhang24@gmail.com>
2019-03-17 18:48:56 +0700

    Update token file with some new functions, support for handling with token

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,32 +1,86 @@
 import { constant } from "./Constant";
+import { toastUtil } from "./ToastUtil";
 
 const updateOrCreateHeader = header => {
-    if (header === null || header === undefined) {
-        header = {
-            method: 'GET',
-            headers: {
-            }
-        }
-    }
-    let token = window.localStorage.getItem(constant.TOKEN_VARIABLE_NAME);
-    if (token === null) {
-        //      window.location= '#login';
-    }
-    if (header.headers === null || header.headers === undefined) {
-        header.headers = {};
-    }
-    header.headers[constant.TOKEN_HEADER_KEY] = token;
-    console.log(header);
-    return header;
+	if (header === null || header === undefined) {
+		header = {
+			method: 'GET',
+			headers: {
+			}
+		}
+	}
+	let token = localStorage.getItem(constant.TOKEN_VARIABLE_NAME);
+	if (token === null) {
+		window.location = constant.ROUTE_HOME;
+	}
+	if (header.headers === null || header.headers === undefined) {
+		header.headers = {};
+	}
+	header.headers[constant.TOKEN_HEADER_KEY] = token;
+	return header;
 }
 
+/**
+ * check authorized status.
+ * @param {*} responseData 
+ */
 const checkAuthorizedStatus = responseData => {
-    if (responseData && responseData.status === 401) {
-        window.location = '#login';
-    }
+	if (responseData && responseData.status === 401) {
+		window.location = constant.ROUTE_HOME;
+	}
+}
+
+/**
+ * check if the response data has error. Use this method to check reponse data from API
+ * @param {*} data 
+ */
+const checkResponseErrorCode = (data) => {
+	if (data && data.errorCode > 0) {
+		if (data.errorCode === 401) {
+			// due to token is expired
+			// should remove token
+			setTimeout(async () => {
+				await _clearAllSavedData();
+				window.location.reload();
+			}, 0)
+		}
+		else {
+			toastUtil.showErrorMsg(data.message)
+		}
+	}
+
+}
+
+/**
+ * To clear all saved data in client browser
+ */
+const _clearAllSavedData = () => {
+	localStorage.clear();
+}
+
+/**
+ * Get saved token
+ */
+const getSavedToken = () => {
+	let token = localStorage[constant.TOKEN_VARIABLE_NAME];
+	if (token === void 0 || token === null) {
+		return null;
+	} else {
+		return token;
+	}
+}
+
+/**
+ * Remove saved token
+ */
+const removeSavedToken = () => {
+	delete localStorage[constant.TOKEN_VARIABLE_NAME];
 }
 
 export const tokenUtil = {
-    updateOrCreateHeader,
-    checkAuthorizedStatus
+	updateOrCreateHeader,
+	checkAuthorizedStatus,
+	getSavedToken,
+	removeSavedToken,
+	checkResponseErrorCode,
 };
