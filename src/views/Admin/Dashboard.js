73469db540db51b154c325bf85b57d07e2f9138c
ktKhang <letankhang24@gmail.com>
2019-03-23 14:41:00 +0700

    Update admin dashboard. Use GGMap for it

--- src/views/Admin/Dashboard.js
+++ src/views/Admin/Dashboard.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Map from "./Map";
 import ShareMap from "./ShareMap";
+import GGMap from '../../utils/GGMap v2.0/GGMap';
 class Dashboard extends Component {
    render() {
       return (
          <div style={{ height: window.innerHeight - 108, minHeight: '518px' }}>
-            <div className="app-content" style={{ width: '100%', height: '100%' }}>
-               <Map style={{ width: '-webkit-fill-available', maxWidth: '-webkit-fill-available', webkitAnimationName: 'example' }}></Map>
+            <div className="admin-app-content">
+               <GGMap
+                  className='admin-map'
+               />
             </div>
          </div>
       );
